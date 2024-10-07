f4e1319c7712ab510ce37b9652e4d73f1944255a
LamphuongZ <lamphuong024@gmail.com>
2024-10-08 01:54:08 +0700

    feat: not-found-page

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -6,15 +6,15 @@ export default function Header() {
   return (
     <div className="px-[112px] py-[24px]">
       <div className="flex items-center capitalize justify-evenly">
-        <div className="flex items-center gap-3">
-          <div className="w-10 h-10">
-            <img src={logo} alt="" className="w-full h-full" />
-          </div>
-          <h1 className="text-3xl font-bold">Việc làm dễ</h1>
-        </div>
-        <ul className="flex items-center gap-10 text-2xl font-medium px-48">
+        {/* <div className="flex justify-center items-center gap-2"> */}
+          <Link to="/" className="flex justify-center items-center gap-2">
+            <img src={logo} alt="" className="w-10 h-10" loading="lazy" />
+            <h1 className="text-[28px] w-60 h-full  font-bold">Việc làm dễ</h1>
+          </Link>
+        {/* </div> */}
+        <ul className="flex justify-center items-center gap-10 text-2xl font-medium px-48">
           <li>
-            <Link to="/">Tìm việc</Link>
+            <Link to="/find-job">Tìm việc</Link>
           </li>
           <li>
             <Link to="/">Chính sách</Link>
--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,16 +1,20 @@
 import { useNavigate } from "react-router-dom";
+import bgImage from "../../assets/gifs/404.gif";
 import Button from "../../components/button";
 
 export default function NotFoundPage() {
   const navigate = useNavigate();
   return (
-    <div className="container-wrapper">
-      <div className="">
-        <h1>404</h1>
+    <div
+      className="container-wrapper relative h-[700px] bg-no-repeat bg-center"
+      style={{ backgroundImage: `url(${bgImage})`, backgroundSize: "inherit" }}
+    >
+      <div className="error mt-10 absolute left-1/2 top-1/5 text-center transform -translate-x-1/2 rotate-10 ">
+        <h1 className="text-slate-900 text-9xl font-bold">404</h1>
       </div>
-      <div className="content">
-        <h1>Thông tin đang được cập nhật</h1>
-        <Button title="Về Trang Chủ" onClick={() => navigate("/")} />
+      <div className="content absolute left-1/2 top-[70%] text-center transform -translate-x-1/2">
+        <h1 className="text-slate-900 text-3xl font-bold">Thông tin đang được cập nhật</h1>
+        <Button margin="20px" title="Về Trang Chủ" onClick={() => navigate("/")} />
       </div>
     </div>
   );

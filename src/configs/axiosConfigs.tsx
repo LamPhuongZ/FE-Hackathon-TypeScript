647adc552730aca05955aab267c02dd2aa4f8a04
LamphuongZ <lamphuong024@gmail.com>
2024-10-07 14:51:14 +0700

    update: install-lir

---
+++ src/configs/axiosConfigs.tsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { DOMAIN, TOKEN_KEY } from "../utils/settings";
+
+const instance = axios.create({
+  baseURL: DOMAIN,
+  headers: {
+    "ngrok-skip-browser-warning": "69420",
+  },
+});
+
+// Cấu hình headers trước khi gửi lên server:
+instance.interceptors.request.use((config) => {
+  const isLogin = localStorage.getItem(TOKEN_KEY) ? true : false;
+  config.headers.Authorization = isLogin
+    ? `Bearer ${localStorage.getItem(TOKEN_KEY)}`
+    : "";
+  return config;
+});
+
+// Tìm lỗi "401" bằng console.log (error)
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      window.location.href = `/login?redirectUrl=${window.location.pathname}`;
+    }
+    return Promise.reject(error);
+  }
+);

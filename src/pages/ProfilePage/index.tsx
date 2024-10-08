47cd63a6ce59a08112318698a93a16d42962d209
taytruong <taytruong05@gmail.com>
2024-10-09 05:16:57 +0700

    update: profile css

--- src/pages/ProfilePage/index.tsx
+++ src/pages/ProfilePage/index.tsx
@@ -4,8 +4,18 @@ type Props = {};
 
 export default function ProfilePage({}: Props) {
     return (
-      <>
-        
-      </>
+      <div className="py-4 px-[72px]">
+        <div className="bg-white">
+          <div className="mb-10 flex items-start justify-between">
+            <div>
+              <h1 className="text-[40px] font-semibold">Thông tin tài khoản</h1>
+              <p className="text-xl font-semibold text-[rgba(10,_22,_41,_0.50)]">Cập nhật thông tin tài khoản</p>
+            </div>
+          </div>
+          <form>
+            <div className="w-[200px] h-[200px] rounded-full mx-auto mb-10"></div>
+          </form>
+        </div>
+      </div>
     );
   }

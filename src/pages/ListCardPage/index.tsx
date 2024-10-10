49fc42a905093b3589f6e7ab88c7b438cc07a941
TayTruong <taytruong05@gmail.com>
2024-10-10 17:56:01 +0700

    build: text

--- src/pages/ListCardPage/index.tsx
+++ src/pages/ListCardPage/index.tsx
@@ -9,7 +9,7 @@ export default function ListCardPage({}: Props) {
     <div className="py-20 px-[72px]">
       <div className="bg-white py-4 shadow-md px-11">
         <h1 className="text-[40px] font-bold mb-5">
-          Danh sách cộng việc đã hoàn thành
+          Danh sách công việc đã hoàn thành
         </h1>
         <div className="w-[779px] border-[3px] border-solid border-[#2EE498] "></div>
 

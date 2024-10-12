e12d4f27da9c524351a0f4fa85c542821513b093
LamphuongZ <lamphuong024@gmail.com>
2024-10-12 22:45:02 +0700

    update: merge-master

--- dist/assets/Button-Ct9pZYiE.js
+++ dist/assets/Button-Ct9pZYiE.js
@@ -1,4 +1,4 @@
-import{aA as t}from"./index-VRVpNQmS.js";function o({title:s,loading:a,className:l,type:n="button",onClick:x,icon:e,iconPosition:r="left"}){return t.jsxs("button",{type:n,className:`cursor-pointer transition-all bg-[#3F8CFF] text-white px-6 py-2 rounded-xl
+import{an as t}from"./index-C9AtbmeP.js";function o({title:s,loading:a,className:l,type:n="button",onClick:x,icon:e,iconPosition:r="left"}){return t.jsxs("button",{type:n,className:`cursor-pointer transition-all bg-[#3F8CFF] text-white px-6 py-2 rounded-xl
     border-blue-600
     border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px]
     active:border-b-[2px] active:brightness-90 active:translate-y-[2px] text-2xl font-bold ${l}`,onClick:x,disabled:a,children:[e&&r==="left"&&t.jsx("span",{className:"mr-2",children:e}),s,e&&r==="right"&&t.jsx("span",{className:"ml-2",children:e})]})}export{o as B};

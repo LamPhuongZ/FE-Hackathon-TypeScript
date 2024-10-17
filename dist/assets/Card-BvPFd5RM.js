e2cda56368341859eb3b2261479a8aeaf24a2ecf
LamphuongZ <lamphuong024@gmail.com>
2024-10-17 15:29:04 +0700

    update: responsive-lading-page

--- dist/assets/Card-BvPFd5RM.js
+++
@@ -1,1 +0,0 @@
-import{am as t}from"./index-BVeeF10x.js";function o({children:e,className:r,onClick:s}){return t.jsx("div",{className:`flex flex-col justify-center ${r} `,onClick:s,children:e})}export{o as C};

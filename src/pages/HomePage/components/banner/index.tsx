ec09d4f6528d23289771e196da49d3d183e5b123
LamphuongZ <lamphuong024@gmail.com>
2024-10-21 14:43:24 +0700

    update: banner-v4

--- src/pages/HomePage/components/banner/index.tsx
+++ src/pages/HomePage/components/banner/index.tsx
@@ -1,6 +1,8 @@
+import { FilterOutlined, SearchOutlined } from "@ant-design/icons";
 import banner1 from "../../../../assets/images/banner1.png";
 import banner2 from "../../../../assets/images/banner2.png";
-import Input from "../../../../components/input/Input";
+import Field from "../../../../components/field/Field";
+import Label from "../../../../components/label/Label";
 
 export default function Banner() {
   return (
@@ -19,8 +21,47 @@ export default function Banner() {
 
       <div className="banner__group">
         <div className="banner__search">
-          
-          {/* <Input name="search" placeholder="Tìm kiếm" control={control} /> */}
+          <div className="">
+            <Field>
+              <div className="top-feature-group flex-1 relative">
+                <div className="absolute left-3 top-[70%] transform -translate-y-1/2">
+                  <SearchOutlined
+                    className="text-gray-500"
+                    style={{ fontSize: "24px" }}
+                  />
+                </div>
+                <input
+                  type="text"
+                  name="search"
+                  placeholder="Tìm kiếm"
+                  className="border rounded-md pl-[20px] pr-12 py-[16px] w-full"
+                />
+              </div>
+            </Field>
+
+            {/* <Field>
+              <div className="top-feature-group flex-1 relative">
+                <Label htmlFor="chooseType">Loại Công Việc</Label>
+                <div className="absolute right-3 top-[70%] transform -translate-y-1/2">
+                  <FilterOutlined
+                    className="text-gray-500"
+                    style={{ fontSize: "24px" }}
+                  />
+                </div>
+                <input
+                  type="text"
+                  name="chooseType"
+                  placeholder="Loại Công Việc"
+                  className="border rounded-md pl-[20px] pr-12 py-[16px] w-full"
+                />
+              </div>
+            </Field>
+            <Field>
+              <p className="text-2xl font-bold pt-11">
+                Khu vực: Thành phố HỒ CHÍ MINH
+              </p>
+            </Field> */}
+          </div>
         </div>
       </div>
     </div>

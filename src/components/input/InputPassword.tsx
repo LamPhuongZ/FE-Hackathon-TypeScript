d1edc84aea61b26b2061ef7c72433770d87b197b
taytruong <taytruong05@gmail.com>
2024-10-29 10:38:04 +0700

    update: fix inputpassword

--- src/components/input/InputPassword.tsx
+++ src/components/input/InputPassword.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useState } from "react";
+import React, { ReactNode } from "react";
 import IconEyeOpen from "../iconEye/IconEyeOpen";
 import IconEyeClose from "../iconEye/IconEyeClose";
 import { useController } from "react-hook-form";

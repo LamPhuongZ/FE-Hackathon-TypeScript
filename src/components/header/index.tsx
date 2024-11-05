1e860c663fe7f6944bf588c0deb06447df4cc987
Jerry Giang Trang <trangtruonggiang2015@gmail.com>
2024-11-05 11:47:23 +0700

    Responsive header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -13,6 +13,7 @@ import { ACCESS_TOKEN } from "../../utils/config";
 import { LuUserCircle2 } from "react-icons/lu";
 import { useRole } from "../../hooks/useRole";
 import { UserRole } from "../../enums/role.enum";
+import { UnorderedListOutlined } from "@ant-design/icons";
 
 export default function Header() {
   const navigate = useNavigate();
@@ -29,7 +30,7 @@ export default function Header() {
     }
   };
 
-  const items: MenuProps["items"] = [
+  const itemsProfile = [
     {
       key: "1",
       style: { background: "none" },
@@ -53,8 +54,6 @@ export default function Header() {
     },
   ];
 
-  
-
   const getMe = async () => {
     const actionAPI = await getProfileAPI();
     dispatch(actionAPI);
@@ -70,107 +69,216 @@ export default function Header() {
   }, []);
 
   // Dropdown menu header button
-  
-
-  return (
-    <>
-    <header className="header w-full">
-      <div className="header__group">
-        <Link to="/" className="link__logo">
-          <img
-            src={logo}
-            alt="icon-logo"
-            width={40}
-            height={40}
-            loading="lazy"
-          />
-          <h1 className="title">Việc làm dễ</h1>
-        </Link>
-        <nav className="nav">
-          <ul className="menu">
-            {token && isEmployer ? (
-              <Link to="/form-application">
-                <Button
-                  title="Đăng bài"
-                  color="custom"
-                  className="bg-[#2EE498] w-40"
-                  circle={false}
-                />
-              </Link>
-            ) : (
-              <li>
-                <Link
-                  to="/list-job/:jobId?section=listJob"
-                  onClick={() => handleScrollToSection}
-                >
-                  Tìm việc
-                </Link>
-              </li>
-            )}
-            <li>
-              <Link
-                to="/policy-page?section=policy"
-                onClick={() => handleScrollToSection}
-              >
-                Chính sách
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/landing-page?section=about"
-                onClick={() => handleScrollToSection}
-              >
-                Về chúng tôi
-              </Link>
-            </li>
-          </ul>
-          <div className="btn__auth">
-            {!userProfile ? (
+  const itemsHeader = [
+    {
+      label: (
+        <>
+          {token && isEmployer ? (
+            <Link to="/form-application">
               <Button
-                title="Đăng nhập / Đăng ký"
+                title="Đăng bài"
                 color="custom"
-                className="btn__login"
-                onClick={() => navigate("/login")}
+                className="bg-[#2EE498] w-40"
                 circle={false}
               />
-            ) : (
-              <Dropdown menu={{ items }}>
-                <div className="flex items-center gap-2">
-                  <div className="w-[50px] h-[50px] rounded-full overflow-hidden">
-                    <img
-                      src={userProfile.avatar ||"https://via.placeholder.com/150"}
-                      alt="avatar"
-                      className="w-full"
-                      loading="lazy"
-                    />
-                  </div>
-                  <a className="cursor-pointer max-w-[180px]">
-                    <Space
-                      className="font-bold leading-5 overflow-hidden "
-                      style={{ maxWidth: "180px" }}
-                    >
-                      <p className="whitespace-nowrap !text-ellipsis">
-                        {userProfile.fullname}
-                      </p>
-                    </Space>
-                  </a>
-                </div>
-              </Dropdown>
-            )}
-          </div>
-        </nav>
-      </div>
-    </header>
-
-    {/* <Dropdown menu={{ itemsHeader }} trigger={['click']}>
-    <a onClick={(e) => e.preventDefault()}>
-      <Space>
-        Click me
-      </Space>
-    </a>
-  </Dropdown> */}
-
-    </>
+            </Link>
+          ) : (
+            <li>
+              <Link
+                to="/list-job/:jobId?section=listJob"
+                onClick={() => handleScrollToSection}
+              >
+                Tìm việc
+              </Link>
+            </li>
+          )}
+        </>
+      ),
+      key: "0",
+    },
+    {
+      label: (
+        <>
+          <Link
+            to="/policy-page?section=policy"
+            onClick={() => handleScrollToSection}
+          >
+            Chính sách
+          </Link>
+        </>
+      ),
+      key: "1",
+    },
     
+    {
+      label: (
+        <>
+          <Link
+            to="/landing-page?section=about"
+            onClick={() => handleScrollToSection}
+          >
+            Về chúng tôi
+          </Link>
+        </>
+      ),
+      key: "3",
+    },
+    {
+      type: "divider",
+    },
+    {
+      label: (
+        <>
+          {!userProfile ? (
+            <Button
+              title="Đăng nhập / Đăng ký"
+              color="delete"
+              className="btn__login"
+              onClick={() => navigate("/login")}
+              circle={false}
+            />
+          ) : (
+            <Dropdown menu={{ items: itemsProfile }}>
+              <div className="flex items-center gap-2">
+                <div className="w-[50px] h-[50px] rounded-full overflow-hidden">
+                  <img
+                    src={
+                      userProfile.avatar || "https://via.placeholder.com/150"
+                    }
+                    alt="avatar"
+                    className="w-full"
+                    loading="lazy"
+                  />
+                </div>
+                <a className="cursor-pointer max-w-[180px]">
+                  <Space
+                    className="font-bold leading-5 overflow-hidden "
+                    style={{ maxWidth: "180px" }}
+                  >
+                    <p className="whitespace-nowrap !text-ellipsis">
+                      {userProfile.fullname}
+                    </p>
+                  </Space>
+                </a>
+              </div>
+            </Dropdown>
+          )}
+        </>
+      ),
+      key: "4",
+    },
+  ];
+  return (
+    <>
+      <header className="header w-full !hidden sm:!block">
+        <div className="header__group">
+          <Link to="/" className="link__logo">
+            <img
+              src={logo}
+              alt="icon-logo"
+              width={40}
+              height={40}
+              loading="lazy"
+            />
+            <h1 className="title">Việc làm dễ</h1>
+          </Link>
+          <nav className="nav">
+            <ul className="menu">
+              {token && isEmployer ? (
+                <Link to="/form-application">
+                  <Button
+                    title="Đăng bài"
+                    color="custom"
+                    className="bg-[#2EE498] w-40"
+                    circle={false}
+                  />
+                </Link>
+              ) : (
+                <li>
+                  <Link
+                    to="/list-job/:jobId?section=listJob"
+                    onClick={() => handleScrollToSection}
+                  >
+                    Tìm việc
+                  </Link>
+                </li>
+              )}
+              <li>
+                <Link
+                  to="/policy-page?section=policy"
+                  onClick={() => handleScrollToSection}
+                >
+                  Chính sách
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/landing-page?section=about"
+                  onClick={() => handleScrollToSection}
+                >
+                  Về chúng tôi
+                </Link>
+              </li>
+            </ul>
+            <div className="btn__auth">
+              {!userProfile ? (
+                <Button
+                  title="Đăng nhập / Đăng ký"
+                  color="custom"
+                  className="btn__login"
+                  onClick={() => navigate("/login")}
+                  circle={false}
+                />
+              ) : (
+                <Dropdown menu={{ items: itemsProfile }}>
+                  <div className="flex items-center gap-2">
+                    <div className="w-[50px] h-[50px] rounded-full overflow-hidden">
+                      <img
+                        src={
+                          userProfile.avatar ||
+                          "https://via.placeholder.com/150"
+                        }
+                        alt="avatar"
+                        className="w-full"
+                        loading="lazy"
+                      />
+                    </div>
+                    <a className="cursor-pointer max-w-[180px]">
+                      <Space
+                        className="font-bold leading-5 overflow-hidden "
+                        style={{ maxWidth: "180px" }}
+                      >
+                        <p className="whitespace-nowrap !text-ellipsis">
+                          {userProfile.fullname}
+                        </p>
+                      </Space>
+                    </a>
+                  </div>
+                </Dropdown>
+              )}
+            </div>
+          </nav>
+        </div>
+      </header>
+      <header className="header w-full !flex sm:!hidden  gap-3 flex-col items-center justify-center">
+        <div>
+          <Link to="/" className="link__logo flex items-center gap-2">
+            <img
+              src={logo}
+              alt="icon-logo"
+              width={40}
+              height={40}
+              loading="lazy"
+            />
+            <h1 className="title font-bold text-2xl">Việc làm dễ</h1>
+          </Link>
+        </div>
+        <Dropdown className="self-start w-full" menu={{ items: itemsHeader }} trigger={["click"]}>
+          <a className="!bg-blue-400 p-3 rounded" onClick={(e) => e.preventDefault()}>
+            <Space><UnorderedListOutlined /></Space>
+          </a>
+        </Dropdown>
+      </header>
+    </>
   );
 }

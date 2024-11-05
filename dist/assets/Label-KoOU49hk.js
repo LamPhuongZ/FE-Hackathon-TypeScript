4e12a309fe0c7b457b0b48270e13145d31ea5e58
LamphuongZ <lamphuong024@gmail.com>
2024-11-05 13:53:35 +0700

    build: run-build

---
+++ dist/assets/Label-KoOU49hk.js
@@ -0,0 +1,1 @@
+import{K as r}from"./index-LyLtH85X.js";function a({htmlFor:t="",children:e,marginBottom:o="mb-8"}){return r.jsx("label",{htmlFor:t,className:`font-semibold cursor-pointer text-2xl  ${o}`,children:e})}export{a as L};
